b4f9bb8455da76cfdeb0d1d3199e7b2b75c78f0d
enok._.seth <84675089+enokseth@users.noreply.github.com>
2023-10-02 17:26:36 +0200

    Update rename-photo.sh
    addes case if is already renamed skip

--- renamephoto/rename-photo.sh
+++ renamephoto/rename-photo.sh
@@ -12,11 +12,17 @@ rename_photos_recursive() {
         elif [ -f "$fichier" ]; then
             # Vérifie s'il s'agit d'un fichier image (vous pouvez ajuster les extensions)
             if [[ "$fichier" =~ \.(jpg|jpeg|png|gif|bmp)$ ]]; then
-                # Renomme le fichier en ajoutant le numéro de séquence
-                extension="${fichier##*.}"
-                nouveau_nom="$repertoire/$compteur.$extension"
-                mv "$fichier" "$nouveau_nom"
-                ((compteur++))
+                # Extrait le nom de base du fichier (sans le chemin)
+                nom_base=$(basename "$fichier")
+
+                # Vérifie si le nom de base commence par un numéro de séquence suivi d'un point
+                if [[ ! "$nom_base" =~ ^[0-9]+\..*$ ]]; then
+                    # Renomme le fichier en ajoutant le numéro de séquence
+                    extension="${fichier##*.}"
+                    nouveau_nom="$repertoire/$compteur.$extension"
+                    mv "$fichier" "$nouveau_nom"
+                    ((compteur++))
+                fi
             fi
         fi
     done
@@ -33,7 +39,7 @@ repertoire_specifie="$1"
 # Vérifie si le répertoire spécifié existe
 if [ -d "$repertoire_specifie" ]; then
     rename_photos_recursive "$repertoire_specifie"
-    echo "Rename recursive is ended successfuly $repertoire_specifie."
+    echo "Renommage récursif terminé dans le répertoire $repertoire_specifie."
 else
-    echo "No repertorie specified."
+    echo "Le répertoire spécifié n'existe pas."
 fi
